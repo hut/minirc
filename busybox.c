8abc9c12ab34bba2c8564ab70394eee3c25f9fe3
hut <hut@lavabit.com>
2013-05-02 17:42:51 +0200

    explain the point of busybox.c

--- busybox.c
+++ busybox.c
@@ -1,3 +1,10 @@
+/*
+ * This code can be used to run "busybox init" with a program of an
+ * arbitrary name, without being forced to call it "init".
+ *
+ * Compile it with "gcc busybox.c -o busybox" and use that binary
+ * for the init kernel parameter instead.
+ */
 #include <unistd.h>
 main() {
   execl("/bin/busybox", "init", (char *) 0);
